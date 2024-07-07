bdff11b8c44622b8139ac89173e60034cdf8177f
Igor Shadurin <igor.shadurin@gmail.com>
2024-07-07 13:41:29 +0100

    feat: addition and subtraction up to 20

--- js/script.js
+++ js/script.js
@@ -1,36 +1,89 @@
-function generateTask(number, isMultiplication) {
-    const randomNum = Math.floor(Math.random() * 12) + 1; // Ensure the random number is between 1 and 12
-    const product = number * randomNum; // The result of multiplication which will always be a positive integer
+const types = ['multiplication', 'division', 'addition', 'subtraction'];
 
-    const multiplicationFormats = [
-        {question: `_ = ${number} x ${randomNum}`, answer: product},
-        {question: `_ = ${randomNum} x ${number}`, answer: product},
-        {question: `${number} x ${randomNum} = _`, answer: product},
-        {question: `${randomNum} x ${number} = _`, answer: product},
-        {question: `${product} = _ x ${number}`, answer: randomNum},
-        {question: `${product} = _ x ${randomNum}`, answer: number},
-        {question: `${product} = ${number} x _`, answer: randomNum},
-        {question: `${product} = ${randomNum} x _`, answer: number},
-        {question: `${number} x _ = ${product}`, answer: randomNum},
-        {question: `${randomNum} x _ = ${product}`, answer: number},
-        {question: `_ x ${number} = ${product}`, answer: randomNum},
-        {question: `_ x ${randomNum} = ${product}`, answer: number},
-    ];
+const getRandomNumber = (max) => Math.floor(Math.random() * max) + 1;
 
-    const divisionFormats = [
-        {question: `_ = ${product} ÷ ${number}`, answer: randomNum},
-        {question: `_ = ${product} ÷ ${randomNum}`, answer: number},
-        {question: `${product} ÷ ${number} = _`, answer: randomNum},
-        {question: `${product} ÷ ${randomNum} = _`, answer: number},
-        {question: `${randomNum} = ${product} ÷ _`, answer: number},
-        {question: `${number} = ${product} ÷ _`, answer: randomNum},
-        {question: `${product} ÷ _ = ${number}`, answer: randomNum},
-        {question: `${product} ÷ _ = ${randomNum}`, answer: number},
-        {question: `_ ÷ ${number} = ${randomNum}`, answer: product},
-        {question: `_ ÷ ${randomNum} = ${number}`, answer: product},
-    ];
+/**
+ * Generates a math task based on the specified type and number
+ * @param {number} number - The base number for the math task
+ * @param {string} type - The type of math task ('multiplication', 'division', 'addition', 'subtraction')
+ * @returns {object} - An object containing the question and the answer
+ */
+function generateTask(number, type) {
+    let randomNum, result, formats;
 
-    const formats = isMultiplication ? multiplicationFormats : divisionFormats;
+    switch (type) {
+        case 'multiplication':
+            randomNum = getRandomNumber(12);
+            result = number * randomNum;
+            formats = [
+                { question: `_ = ${number} x ${randomNum}`, answer: result },
+                { question: `_ = ${randomNum} x ${number}`, answer: result },
+                { question: `${number} x ${randomNum} = _`, answer: result },
+                { question: `${randomNum} x ${number} = _`, answer: result },
+                { question: `${result} = _ x ${number}`, answer: randomNum },
+                { question: `${result} = _ x ${randomNum}`, answer: number },
+                { question: `${result} = ${number} x _`, answer: randomNum },
+                { question: `${result} = ${randomNum} x _`, answer: number },
+                { question: `${number} x _ = ${result}`, answer: randomNum },
+                { question: `${randomNum} x _ = ${result}`, answer: number },
+                { question: `_ x ${number} = ${result}`, answer: randomNum },
+                { question: `_ x ${randomNum} = ${result}`, answer: number },
+            ];
+            break;
+        case 'division':
+            randomNum = getRandomNumber(12);
+            result = number * randomNum;
+            formats = [
+                { question: `_ = ${result} ÷ ${number}`, answer: randomNum },
+                { question: `_ = ${result} ÷ ${randomNum}`, answer: number },
+                { question: `${result} ÷ ${number} = _`, answer: randomNum },
+                { question: `${result} ÷ ${randomNum} = _`, answer: number },
+                { question: `${randomNum} = ${result} ÷ _`, answer: number },
+                { question: `${number} = ${result} ÷ _`, answer: randomNum },
+                { question: `${result} ÷ _ = ${number}`, answer: randomNum },
+                { question: `${result} ÷ _ = ${randomNum}`, answer: number },
+                { question: `_ ÷ ${number} = ${randomNum}`, answer: result },
+                { question: `_ ÷ ${randomNum} = ${number}`, answer: result },
+            ];
+            break;
+        case 'addition':
+            randomNum = getRandomNumber(10);
+            result = number + randomNum;
+            formats = [
+                { question: `_ = ${number} + ${randomNum}`, answer: result },
+                { question: `_ = ${randomNum} + ${number}`, answer: result },
+                { question: `${number} + ${randomNum} = _`, answer: result },
+                { question: `${randomNum} + ${number} = _`, answer: result },
+                { question: `${result} = _ + ${number}`, answer: randomNum },
+                { question: `${result} = _ + ${randomNum}`, answer: number },
+                { question: `${result} = ${number} + _`, answer: randomNum },
+                { question: `${result} = ${randomNum} + _`, answer: number },
+                { question: `${number} + _ = ${result}`, answer: randomNum },
+                { question: `${randomNum} + _ = ${result}`, answer: number },
+                { question: `_ + ${number} = ${result}`, answer: randomNum },
+                { question: `_ + ${randomNum} = ${result}`, answer: number },
+            ];
+            break;
+        case 'subtraction':
+            randomNum = getRandomNumber(10);
+            result = number + randomNum;
+            formats = [
+                { question: `_ = ${result} - ${number}`, answer: randomNum },
+                { question: `_ = ${result} - ${randomNum}`, answer: number },
+                { question: `${result} - ${number} = _`, answer: randomNum },
+                { question: `${result} - ${randomNum} = _`, answer: number },
+                { question: `${randomNum} = ${result} - _`, answer: number },
+                { question: `${number} = ${result} - _`, answer: randomNum },
+                { question: `${result} - _ = ${number}`, answer: randomNum },
+                { question: `${result} - _ = ${randomNum}`, answer: number },
+                { question: `_ - ${number} = ${randomNum}`, answer: result },
+                { question: `_ - ${randomNum} = ${number}`, answer: result },
+            ];
+            break;
+        default:
+            throw new Error('Invalid type. Accepted values are multiplication, division, addition, subtraction');
+    }
+
     const position = Math.floor(Math.random() * formats.length);
     const task = formats[position];
     task.question = task.question.replace('_', '<span id="answer"></span>');
@@ -38,6 +91,12 @@ function generateTask(number, isMultiplication) {
     return task;
 }
 
+/**
+ * Checks the user's solution for a given math question
+ * @param {string} question - The math question
+ * @param {number} userAnswer - The user's answer
+ * @returns {boolean} - Whether the user's answer is correct
+ */
 function checkMathSolution(question, userAnswer) {
     const cleanQuestion = question.replace('<span id="answer"></span>', userAnswer);
     const [lhs, rhs] = cleanQuestion.split('=').map(part => part.trim());
@@ -59,6 +118,10 @@ function checkMathSolution(question, userAnswer) {
                 return operands[0] * operands[1];
             case '÷':
                 return operands[0] / operands[1];
+            case '+':
+                return operands[0] + operands[1];
+            case '-':
+                return operands[0] - operands[1];
             default:
                 return null; // Unexpected operator
         }
